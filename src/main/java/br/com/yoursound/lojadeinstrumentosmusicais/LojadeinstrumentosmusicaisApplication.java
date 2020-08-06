064061739bfc42231d3885f9f1e673287a14ed2b
Mateus Lima <mateus.lima@osf.digital>
2020-08-06 20:54:14 -0300

    adding template

---
+++ src/main/java/br/com/yoursound/lojadeinstrumentosmusicais/LojadeinstrumentosmusicaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.yoursound.lojadeinstrumentosmusicais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojadeinstrumentosmusicaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojadeinstrumentosmusicaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/yoursound/lojadeinstrumentosmusicais/LojadeinstrumentosmusicaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.yoursound.lojadeinstrumentosmusicais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojadeinstrumentosmusicaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
